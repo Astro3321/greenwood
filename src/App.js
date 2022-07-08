08fcd7ec66d90231c54dcfe8e6ff4c8da650cfc8
Ashish Chauhan <ashishchauhan3301@gmail.com>
2022-07-08 21:06:48 +0530

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Container } from "react-bootstrap"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { AuthProvider } from "./contexts/AuthContext"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Container fluid>
+    <Router>
+      <AuthProvider>
+		  <Routes>
+				<Route path="/" element={}></Route> {/* Home */}
+				<Route path="/login" element={}></Route> {/* Login */}
+				<Route path="/signup" element={}></Route> {/* Signup */}
+		  </Routes>
+	  </AuthProvider>
+    </Router>
+  </Container>
 }
 
 export default App;
