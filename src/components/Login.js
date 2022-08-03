1eedf4ce011ddde7ad5e737ab69713cca541260f
Ashish Chauhan <ashishchauhan3301@gmail.com>
2022-08-03 17:36:59 +0530

    Add Login Functionality and Routing

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,33 +1,61 @@
-import React,{useRef} from 'react'
-import {Form, Button, Card } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import ".//UserForm.css"
+import React,{useEffect, useRef, useState} from 'react'
+import {Form, Button, Card, Alert } from 'react-bootstrap'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
+import "./UserForm.css"
 
 export default function Login() {
   const emailRef = useRef()
   const passwordRef = useRef()
-  
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  const { currentUser, login } = useAuth()
+  const navigate = useNavigate()
 
+  useEffect(() => {
+    if (currentUser){
+      console.log(currentUser.uid)
+      console.log(currentUser.email)
+      navigate("/profile")
+    }
+  }, [])
+
+  async function handleSubmit(event){
+    event.preventDefault()
+    
+    try{
+      setError('')
+      setLoading(true)
+
+      await login(emailRef.current.value, passwordRef.current.value)
+
+      navigate("/")
+    } catch(e) {
+      setError(e.message)
+    }
+
+    setLoading(false)
+  }
   
   return (<>
   <br></br>
-  <div class= "square1">
+  <div className="square1">
     <Card>
       <Card.Body>
       <h1 className='text-center mb-4'> Login</h1>
-      
-        <Form>
+      {error && <Alert variant="danger">{error}</Alert>}
+        <Form onSubmit={handleSubmit}>
           <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
           </Form.Group>
 
-          <Form.Group id="passward">
+          <Form.Group id="password">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
           </Form.Group>
           <br></br>
-          <Button className="w-100" type="submit">Login</Button>
+          <Button className="w-100" type="submit" disabled={loading}>Login</Button>
         </Form>
       </Card.Body>
     </Card>
