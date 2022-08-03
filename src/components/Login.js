98fc2ed5190d4d2a51198e0a958452d96439b243
Ashish Chauhan <ashishchauhan3301@gmail.com>
2022-08-03 13:19:24 +0530

    Fix typos, Add Linking between Login.js and Signup.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,34 +1,33 @@
 import React,{useRef} from 'react'
 import {Form, Button, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
   const emailRef = useRef()
-    const passwordRef = useRef()
-  return (
-    <>
+  const passwordRef = useRef()
+
+  return (<>
     <Card>
       <Card.Body>
       <h1 className='text-center mb-4'> Login</h1>
         <Form>
-          <Form.Group ig="email">
+          <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control typr="email" ref={emailRef} required />
+              <Form.Control type="email" ref={emailRef} required />
           </Form.Group>
-          <Form.Group ig="passward">
-              <Form.Label>Password</Form.Label>
-              <Form.Control typr="password" ref={passwordRef} required />
-          </Form.Group>
-          <Button className="w-100" type="submit">Login</Button>
 
+          <Form.Group id="passward">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+          </Form.Group>
+          
+          <Button className="w-100" type="submit">Login</Button>
         </Form>
       </Card.Body>
     </Card>
-<div className="w-100 text-center mt-2">
-    You don't have an account. //Signup
-</div>
-   
-</>
-    
-  )
+    <div className="w-100 text-center mt-2">
+        Need an Account? <Link to="/signup">Sign Up</Link>
+    </div>
+  </>)
 }
  
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,36 +1,38 @@
 import React,{useRef} from 'react'
 import {Form, Button, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default function Signup() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
-  return (
-    <>
+
+    return (<>
     <Card>
         <Card.Body>
-        <h1 className='text-center mb-4'> Sign Up</h1>
-        <Form>
-            <Form.Group ig="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control typr="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group ig="passward">
-                <Form.Label>password</Form.Label>
-                <Form.Control typr="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group ig="password-confirm">
-                <Form.Label>Password Confirmation</Form.Label>
-                <Form.Control typr="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button className="w-100" type="submit">Sign Up</Button>
+            <h1 className='text-center mb-4'> Sign Up</h1>
+            <Form>
+                <Form.Group id="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
 
-        </Form>
+                <Form.Group id="passward">
+                    <Form.Label>password</Form.Label>
+                    <Form.Control type="password" ref={passwordRef} required />
+                </Form.Group>
+
+                <Form.Group id="password-confirm">
+                    <Form.Label>Password Confirmation</Form.Label>
+                    <Form.Control type="password" ref={passwordConfirmRef} required />
+                </Form.Group>
+
+                <Button className="w-100" type="submit">Sign Up</Button>
+            </Form>
         </Card.Body>
     </Card>
     <div className="w-100 text-center mt-2">
-        Already have an account. //Login
+        Already have an Account?<Link to="login">Login</Link>
     </div>
-    </>
-  )
+    </>)
 }
