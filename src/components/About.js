c05bc9abcb1ed4450fdb3c1ad614aa1ea3339a3e
Ashish Chauhan <ashishchauhan3301@gmail.com>
2022-08-05 11:41:19 +0530

    Add About Function and Default Import to About.js, Move JSX to About Function

--- src/components/About.js
+++ src/components/About.js
@@ -1,84 +1,83 @@
 import React from 'react'
 import ".//about.css"
 
+export default function About() {
+    return (<>
+        <div class="about-section">
+            <h1><strong>ABOUT US PAGE</strong></h1>
+            <br/><h3>describtion of the project</h3>
+        </div>
+        <h2 style="text-align:center"><strong>OUR TEAM</strong></h2>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                <img href="test-pic-1.jpg" style ="width:100%"></img> 
+                <div class="container">
+                    <h2>Animesh Raturi</h2>
+                    <br/>Course: B.tech CSE with MLAI
+                    <br/>College: Uttaranchal University
+                    <br/>Email: animeshraturi44@gmail.com
+                </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                <img href="test-pic-2.jpg" style ="width:100%"></img> 
+                <div class="container">
+                    <h2>Animesh2</h2>
+                    <br/>Course: B.tech CSE with MLAI
+                    <br/>College: Uttaranchal University
+                    <br/>Email: animeshraturi44@gmail.com
+                </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                <img href="test-pic-3.jpg" style ="width:100%"></img> 
+                <div class="container">
+                    <h2>Animesh3</h2>
+                    <br/>Course: B.tech CSE with MLAI
+                    <br/>College: Uttaranchal University
+                    <br/>Email: animeshraturi44@gmail.com
+                </div>
+                </div>
+            </div>
+        </div>
 
-retuen(<>
-
-<div class="about-section">
-    <h1><strong>ABOUT US PAGE</strong></h1>
-    <br><h3>describtion of the project</h3></br>
-</div>
-<h2 style="text-align:center"><strong>OUR TEAM</strong></h2>
-<div class="row">
-    <div class="column">
-        <div class="card">
-           <img href="test-pic-1.jpg" style ="width:100%"></img> 
-           <div class="container">
-            <h2>Animesh Raturi</h2>
-            <br>Course: B.tech CSE with MLAI</br>
-            <br>College: Uttaranchal University</br>
-            <br>Email: animeshraturi44@gmail.com</br>
-           </div>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                <img href="test-pic-1.jpg" style ="width:100%"></img> 
+                <div class="container">
+                    <h2>Animesh Raturi</h2>
+                    <br/>Course: B.tech CSE with MLAI
+                    <br/>College: Uttaranchal University
+                    <br/>Email: animeshraturi44@gmail.com
+                </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                <img href="test-pic-2.jpg" style ="width:100%"></img> 
+                <div class="container">
+                    <h2>Animesh2</h2>
+                    <br/>Course: B.tech CSE with MLAI
+                    <br/>College: Uttaranchal University
+                    <br/>Email: animeshraturi44@gmail.com
+                </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                <img href="test-pic-3.jpg" style ="width:100%"></img> 
+                <div class="container">
+                    <h2>Animesh3</h2>
+                    <br/>Course: B.tech CSE with MLAI
+                    <br/>College: Uttaranchal University
+                    <br/>Email: animeshraturi44@gmail.com
+                </div>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="column">
-        <div class="card">
-           <img href="test-pic-2.jpg" style ="width:100%"></img> 
-           <div class="container">
-            <h2>Animesh2</h2>
-            <br>Course: B.tech CSE with MLAI</br>
-            <br>College: Uttaranchal University</br>
-            <br>Email: animeshraturi44@gmail.com</br>
-           </div>
-        </div>
-    </div>
-    <div class="column">
-        <div class="card">
-           <img href="test-pic-3.jpg" style ="width:100%"></img> 
-           <div class="container">
-            <h2>Animesh3</h2>
-            <br>Course: B.tech CSE with MLAI</br>
-            <br>College: Uttaranchal University</br>
-            <br>Email: animeshraturi44@gmail.com</br>
-           </div>
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="column">
-        <div class="card">
-           <img href="test-pic-1.jpg" style ="width:100%"></img> 
-           <div class="container">
-            <h2>Animesh Raturi</h2>
-            <br>Course: B.tech CSE with MLAI</br>
-            <br>College: Uttaranchal University</br>
-            <br>Email: animeshraturi44@gmail.com</br>
-           </div>
-        </div>
-    </div>
-    <div class="column">
-        <div class="card">
-           <img href="test-pic-2.jpg" style ="width:100%"></img> 
-           <div class="container">
-            <h2>Animesh2</h2>
-            <br>Course: B.tech CSE with MLAI</br>
-            <br>College: Uttaranchal University</br>
-            <br>Email: animeshraturi44@gmail.com</br>
-           </div>
-        </div>
-    </div>
-    <div class="column">
-        <div class="card">
-           <img href="test-pic-3.jpg" style ="width:100%"></img> 
-           <div class="container">
-            <h2>Animesh3</h2>
-            <br>Course: B.tech CSE with MLAI</br>
-            <br>College: Uttaranchal University</br>
-            <br>Email: animeshraturi44@gmail.com</br>
-           </div>
-        </div>
-    </div>
-</div>
-
-</>)
+    </>)
+}
