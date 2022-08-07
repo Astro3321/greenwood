a4a72246a95d1349b23ce3e8d0e21b284ffe4d7f
Animesh Raturi <animeshraturi44@gmail.com>
2022-08-07 17:13:32 +0530

    change the styling of about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Col, Card, Row } from 'react-bootstrap'
 import ".//about.css"
 
 export default function About() {
@@ -11,73 +12,128 @@ export default function About() {
         <h2 className='text-center mb-4'><strong>OUR TEAM</strong></h2>
         <div className="row">
             <div className="column">
-                <div className="card">
-                <img  img src="test-pic-1.jpg" alt="" className='square' height={150} width={150}/>
-                <div className="container">
-                    <h2>Animesh Raturi</h2>
-                    <br/>Course: B.tech CSE with MLAI
-                    <br/>College: Uttaranchal University
-                    <br/>Email: animeshraturi44@gmail.com
-                </div>
-                </div>
+                <Card className="card">
+                    <Card.Body>
+                    <Row className='hello'>
+                        <Col className='by'>
+                            <img src="test-pic-1.jpg" alt="" className='square' height={150} width={150}/>
+                        </Col>
+                        <Col>
+                            <div className="container">
+                                <h2>Animesh Raturi</h2>
+                                <br/>Course: B.tech CSE with MLAI
+                                <br/>College: Uttaranchal University
+                                <br/>Email: animeshraturi44@gmail.com
+                            </div>
+                        </Col>
+                    </Row>
+                    </Card.Body>
+                </Card>
             </div>
             <div className="column">
-                <div className="card">
-                <img  img src="test-pic-2.jpg" alt="" className='square' height={150} width={150}/>
-                <div className="container">
-                    <h2>Animesh2</h2>
-                    <br/>Course: B.tech CSE with MLAI
-                    <br/>College: Uttaranchal University
-                    <br/>Email: animeshraturi44@gmail.com
-                </div>
-                </div>
+                <Card className="card">
+                    <Card.Body>
+                    <Row className='hello'>
+                        <Col className='by'>
+                            <img src="test-pic-2.jpg" alt="" className='square' height={150} width={150}/>
+                        </Col>
+                        <Col>
+                            <div className="container">
+                                <h2>Animesh Raturi</h2>
+                                <br/>Course: B.tech CSE with MLAI
+                                <br/>College: Uttaranchal University
+                                <br/>Email: animeshraturi44@gmail.com
+                            </div>
+                        </Col>
+                    </Row>
+                    </Card.Body>
+                </Card>
+            </div>
+            
+        </div>
+        
+        <div className="row">
+            
+        <div className="column">
+                <Card className="card">
+                    <Card.Body>
+                    <Row className='hello'>
+                        <Col className='by'>
+                            <img src="test-pic-3.jpg" alt="" className='square' height={150} width={150}/>
+                        </Col>
+                        <Col>
+                            <div className="container">
+                                <h2>Animesh Raturi</h2>
+                                <br/>Course: B.tech CSE with MLAI
+                                <br/>College: Uttaranchal University
+                                <br/>Email: animeshraturi44@gmail.com
+                            </div>
+                        </Col>
+                    </Row>
+                    </Card.Body>
+                </Card>
             </div>
             <div className="column">
-                <div className="card">
-                <img  img src="test-pic-3.jpg" alt="" className='square' height={150} width={150}/> 
-                <div className="container">
-                    <h2>Animesh3</h2>
-                    <br/>Course: B.tech CSE with MLAI
-                    <br/>College: Uttaranchal University
-                    <br/>Email: animeshraturi44@gmail.com
-                </div>
-                </div>
+                <Card className="card">
+                    <Card.Body>
+                    <Row className='hello'>
+                        <Col className='by'>
+                            <img src="test-pic-1.jpg" alt="" className='rounded-circle' height={150} width={150}/>
+                        </Col>
+                        <Col>
+                            <div className="container">
+                                <h2>Animesh Raturi</h2>
+                                <br/>Course: B.tech CSE with MLAI
+                                <br/>College: Uttaranchal University
+                                <br/>Email: animeshraturi44@gmail.com
+                            </div>
+                        </Col>
+                    </Row>
+                    </Card.Body>
+                </Card>
             </div>
+
         </div>
 
         <div className="row">
-            <div className="column">
-                <div className="card">
-                <img  img src="test-pic-1.jpg" alt="" className='rounded-circle' width={150}/>
-                <div className="container">
-                    <h2>Animesh Raturi</h2>
-                    <br/>Course: B.tech CSE with MLAI
-                    <br/>College: Uttaranchal University
-                    <br/>Email: animeshraturi44@gmail.com
-                </div>
-                </div>
+            
+        <div className="column">
+                <Card className="card">
+                    <Card.Body>
+                    <Row className='hello'>
+                        <Col className='by'>
+                            <img src="test-pic-2.jpg" alt="" className='rounded-circle' height={150} width={150}/>
+                        </Col>
+                        <Col>
+                            <div className="container">
+                                <h2>Animesh Raturi</h2>
+                                <br/>Course: B.tech CSE with MLAI
+                                <br/>College: Uttaranchal University
+                                <br/>Email: animeshraturi44@gmail.com
+                            </div>
+                        </Col>
+                    </Row>
+                    </Card.Body>
+                </Card>
             </div>
             <div className="column">
-                <div className="card">
-                <img  img src="test-pic-2.jpg" alt="" className='rounded-circle' width={150}/>
-                <div className="container">
-                    <h2>Animesh2</h2>
-                    <br/>Course: B.tech CSE with MLAI
-                    <br/>College: Uttaranchal University
-                    <br/>Email: animeshraturi44@gmail.com
-                </div>
-                </div>
-            </div>
-            <div className="column">
-                <div className="card">
-                <img  img src="test-pic-3.jpg" alt="" className='rounded-circle' width={150}/> 
-                <div className="container">
-                    <h2>Animesh3</h2>
-                    <br/>Course: B.tech CSE with MLAI
-                    <br/>College: Uttaranchal University
-                    <br/>Email: animeshraturi44@gmail.com
-                </div>
-                </div>
+                <Card className="card">
+                    <Card.Body>
+                    <Row className='hello'>
+                        <Col className='by'>
+                            <img src="test-pic-3.jpg" alt="" className='rounded-circle' height={150} width={150}/>
+                        </Col>
+                        <Col>
+                            <div className="container">
+                                <h2>Animesh Raturi</h2>
+                                <br/>Course: B.tech CSE with MLAI
+                                <br/>College: Uttaranchal University
+                                <br/>Email: animeshraturi44@gmail.com
+                            </div>
+                        </Col>
+                    </Row>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
     </>)
