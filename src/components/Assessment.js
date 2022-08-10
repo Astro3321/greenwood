02f3c3c48b76357f0b75a415a4374a5700c572bd
Ashish Chauhan <ashishchauhan3301@gmail.com>
2022-08-11 00:21:01 +0530

    Add Database Connectivity, Fetch Questions from Database

--- src/components/Assessment.js
+++ src/components/Assessment.js
@@ -1,28 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Dropdown from 'react-bootstrap/Dropdown'
+import { collection, getDocs, orderBy, query } from "firebase/firestore"
+import { db } from "../config/firebase-config"
 import "./assessment.css"
+
 export default function Assessment() {
-  
+  const questionCollectionRef = collection(db, "autism")
+  const q = query(questionCollectionRef)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const loadData = async () => {
+      const res = await getDocs(q)
+      const docData = res.docs.map((doc) => (
+        doc.data()
+        ))
+        console.log(docData)
+        setData(docData)
+      }
+
+    loadData()
+  }, [])
+
+  const showQuestions = data.map((obj, index) => {
+    return <div className='question-card mx-auto mb-2'>
+      <h3>{`Question ${index+1}. `} {obj.question}</h3>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Answer
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">{obj.option_1}</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">{obj.option_2}</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">{obj.option_3}</Dropdown.Item>
+          <Dropdown.Item href="#/action-4">{obj.option_4}</Dropdown.Item>
+          <Dropdown.Item href="#/action-5">{obj.option_5}</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  })
+
+
   return (
     <div className="Assessment">
     <center> 
       <h1>Quiz A</h1>
     </center>
-   <div className='question-card'>
-    <h3> Question 1. What is 10+1?</h3>
-    <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Answer
-      </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">14</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">24</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">11</Dropdown.Item>
-        <Dropdown.Item href="#/action-4">10</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-   </div>
+    {showQuestions}
+
    </div>
   )
 }
--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,4 +13,5 @@ const config = {
 
 const app = initializeApp(config)
 export const auth = getAuth(app)
+export const db = getFirestore(app, {useFetchStreams: false, experimentalForceLongPolling:true})
 export default app
