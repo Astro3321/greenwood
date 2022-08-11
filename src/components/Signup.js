1c648e6037211291cdedde842bbc6353e1380413
Ashish Chauhan <ashishchauhan3301@gmail.com>
2022-08-11 13:27:45 +0530

    ~Add "SignUp as an Organization" Field in Signup Form ~Add Custom Storage of User Data in Users Collection during Signup Process ~Add Data Fetch Logic in Update Profile from Users Collection ~Changes to Update Profile Form fields ~Convert Bootstrap to React Bootstrap

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,14 +1,17 @@
-import { Alert } from 'bootstrap'
+import { addDoc, collection } from 'firebase/firestore'
 import React,{useRef, useState} from 'react'
-import {Form, Button, Card } from 'react-bootstrap'
+import {Form, Button, Card, Alert } from 'react-bootstrap'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
+import { db } from "../config/firebase-config"
 import ".//UserForm.css"
 
 export default function Signup() {
+    const nameRef = useRef()
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const [userType, setUserType] = useState(null)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState('')
     const navigate = useNavigate()
@@ -21,10 +24,20 @@ export default function Signup() {
             return setError("Passwords do not match")
         }
 
+        const userData = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            displayImage: "",
+            userType: userType
+        }
+
+        const userCollectionRef = collection(db, "users")
+
         try {
             setError('')
             setLoading(true)
             await signup(emailRef.current.value, passwordRef.current.value)
+            await addDoc(userCollectionRef, userData)
             navigate("/")
         } catch(e) {
             setError(e.message)
@@ -40,21 +53,49 @@ export default function Signup() {
         <Card.Body style={{background:"white"}}>
             <h1 className='text-center mb-4'><strong>Sign Up</strong></h1>
             {error && <Alert variant="danger">{error}</Alert>}
+
             <Form onSubmit={handleSubmit}>
+                <Form.Group id="name">
+                    <Form.Label><strong>Name</strong></Form.Label>
+                    <Form.Control type="text" ref={nameRef} required />
+                </Form.Group>
+
                 <Form.Group id="email">
                     <Form.Label><strong>Email</strong></Form.Label>
                     <Form.Control type="email" ref={emailRef} required />
                 </Form.Group>
+
                 <br />
-                <Form.Group id="passward">
+                <Form.Group id="password">
                     <Form.Label><strong>Password</strong></Form.Label>
                     <Form.Control type="password" ref={passwordRef} required />
                 </Form.Group>
+
                 <br />
                 <Form.Group id="password-confirm">
                     <Form.Label><strong>Password Confirmation</strong></Form.Label>
                     <Form.Control type="password" ref={passwordConfirmRef} required />
                 </Form.Group>
+
+                <Form.Label><strong>Signup as an Organization</strong></Form.Label>
+                <Form.Check 
+                    type="radio"
+                    name="userType"
+                    label="Yes"
+                    value="true"
+                    onChange={() => {
+                        setUserType("ORG")
+                    }}
+                    required />
+                <Form.Check
+                    type="radio"
+                    name="userType"
+                    label="No"
+                    value="false"
+                    onChange={() => {
+                        setUserType("IND")
+                    }} 
+                    required />
 
                 <Button className="w-100 mt-4" type="submit" disabled={loading}>Sign Up</Button>
             </Form>
--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -1,67 +1,81 @@
-import React from 'react'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import React, { useEffect, useRef, useState } from 'react'
+import { Form, Button, Card, Alert } from 'react-bootstrap'
+import { db } from '../config/firebase-config'
+import { useAuth } from "../contexts/AuthContext"
 // import CoverImg from "../images/CoverImg.png"
 
 export default function UpdateProfile(props) {
     // const body = {
     //     background: `linear-gradient(rgb(0,0,0,0.5),#28c792),url(${CoverImg}) no-repeat center center / cover`,
     // }
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+    const [image, setImage] = useState()
+    const [data, setData] = useState([])
+    const { currentUser } = useAuth()
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    const userCollectionRef = collection(db, "users")
+    const q = query(userCollectionRef, where("email", '==', `${currentUser.email}`))
+
+    useEffect(() => {
+        const loadData = async () => {
+            const queryData = await getDocs(q)
+            setData(queryData.docs[0].data())
+        }
+
+        loadData()
+    }, [])
+
+    function handleSubmit(){
+        console.log("here")
+    }
 
    
-  return (
-    <div >
-        <div className="card">
-                <div className="card-header" style={{background: "#c2f0c4"}}>
-                    <h2>Update Profile</h2>
-                    {/* <button type='button' className='btn btn-success'> <strong>See All</strong> <span className="las la-arrow-right"></span></button> */} <br />
-                </div> </div>
-      <div className="container" >
-        <div className="row d-flex justify-content-center" >
-            <div className="col-md-10 mt-5 pt-5">
-                <div className="row z-depth-3" style={{border: "2px solid black"}}>
-                    <div className="col-sm-4 bg-info bg-gradient rounded-left" >
-                     <div className="card-block text-center text-white"> <br />
-                     <img src="profile.png" alt="" className='rounded-circle' width={120}/> <br />
-                     <input type="file" name="profilePic" id="ProfilePic" />
-                        <h2 className="font-weight-bold mt-4"><strong>Arpit Kashyap</strong></h2>
-                        <p>XYZ school</p>
-                        <i className="far fa-edit fa-2x mb-4"></i>
-                        </div>   
-                    </div>
-                    <div className="col-sm-8 bg-white rounded-right">
-                        <h3 className="mt-3 text-center" style={{fontweight:"bold",}}>Information</h3>
-                        <hr className="badge-dark mt-0 w-25" />
-                        <div className="row">
-                            <div className="col-sm-6">
-                                <p className="font-weight-bold"><b>Name</b></p>
-                                <input type="text" name="name" id="name"  />
-                            </div>
-                            <div className="col-sm-6">
-                                <p className="font-weight-bold"><b>School</b></p>
-                                <input type="text" name="school" id="school" />
-                            </div>
-                        </div><br />
-                        
-                       
-                        <div className="row">
-                        <div className="col-sm-6">
-                                <p className="font-weight-bold"><b>Email</b></p>
-                                <input type="email" name="email" id="email" />
-                            </div>
-                            <div className="col-sm-6">
-                                <p className="font-weight-bold"><b>Phone</b></p>
-                                <input type="number" name="number" id="number" />
-                            </div>
+    return <>
+            <Card className="mx-auto mt-4 mb-2" style={{width: "23rem", borderRadius: "0.6rem"}}>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Update Profile</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <img src={data.displayImage?data.displayImage:"profile.png"} 
+                            alt={data.name} className="rounded-circle d-flex mx-auto mb-3" style={{width: "40%"}} />
 
-                        </div>
-                        <hr className="bg-dark" />
-                        <button type='button' className='btn btn-success' style={{width:"600px"}}> <strong>Save</strong> <span className="las la-arrow-left"></span></button> <br /><br />
+                        <Form.Group id="display-image">
+                            <Form.Control type="file" accept="/images" onChange={(event) => {
+                                setImage(event.target.files[0])
+                            }} />
+                        </Form.Group>
 
-                    </div>
-                </div>
-            </div>
-        </div>
-      </div>
-      <br /><br /><br /><br /><br />
-    </div>
-  )
+                        <Form.Group id="name">
+                            <Form.Label>Full Name</Form.Label>
+                            <Form.Control type="text" ref={nameRef} required defaultValue={data.name} />
+                        </Form.Group>
+
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required defaultValue={data.email} />
+                        </Form.Group>
+
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} placeholder="Leave Blank to keep same" />
+                        </Form.Group>
+
+                        <Form.Group id="password-confirm">
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control type="password" ref={passwordConfirmRef} placeholder="Leave Blank to keep same" />
+                        </Form.Group>
+
+                        <Button className="w-25 text-center d-flex mx-auto mt-4" disabled={loading} type="submit">
+                            Update
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+    </>
 }
