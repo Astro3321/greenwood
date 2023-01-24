6f72cef1846435072e98476bdb38e5b09cd54f48
Ashish Chauhan <ashishchauhan3301@gmail.com>
2023-01-24 18:17:44 +0530

    Change result assessment and object format

--- src/pages/Assessment/genResult.js
+++ src/pages/Assessment/genResult.js
@@ -1,13 +1,8 @@
-// Returns a object containing disorder category and risk level
-// Object Format: {disorder, riskLevel}
-
-/*  Disorder Category
-    Category 'NU' --> Null 
-    Category 'AU' --> Autism
-    Category 'DX' --> Dyslexia
-*/
+// Returns a object containing Risk Level(RL) for each disorder
+// Object Format: {autismRL, adhdRL, dyslexiaRL, dyspraxiaRL, dysgraphiaRL, dyscalculiaRL}
 
 /*  Risk Levels
+    Category -1 --> N/A
     Category 0 --> No Risk
     Category 1 --> Low Risk
     Category 2 --> Moderate Risk
@@ -15,33 +10,31 @@
 */
 
 export default function genResult(ans){
-    var result = {disorder: 'NU', riskLevel: 0};
-    var autismRisk = 0;
-    var dyslexiaRisk = 0;
+    var result = {
+        autismRL: -1, 
+        adhdRL: -1, 
+        dyslexiaRL: -1, 
+        dyspraxiaRL: -1, 
+        dysgraphiaRL: -1, 
+        dyscalculiaRL: -1
+    };
     
-    //Autism Detection 
+    // Autism Detection 
     const sumAutism = ans.slice(0, 41).reduce((partialSum, a) => partialSum + a, 0)
 
-    if (sumAutism < 70) { autismRisk = 0; }
-    else if (sumAutism > 70 && sumAutism <= 106){ autismRisk = 1; }
-    else if (sumAutism > 107 && sumAutism <= 153){ autismRisk = 2; }
-    else if (sumAutism > 153){ autismRisk = 3; }
+    if (sumAutism < 70) { result.autismRL = 0; }
+    else if (sumAutism > 70 && sumAutism <= 106){ result.autismRL = 1; }
+    else if (sumAutism > 107 && sumAutism <= 153){ result.autismRL = 2; }
+    else if (sumAutism > 153){ result.autismRL = 3; }
     
     // Dyslexia Detection
     const sumDyslexia = ans.slice(41, 71).reduce((partialSum, a) => partialSum + a, 0)
     const probability = (sumDyslexia / 150) * 100
 
-    if (probability <= 25){ dyslexiaRisk = 0; }
-    else if (probability > 25 && probability <= 50){ dyslexiaRisk = 1; }
-    else if (probability > 50 && probability <=75){ dyslexiaRisk = 2; }
-    else if (probability > 75){ dyslexiaRisk = 3; }
-
-    // Risk Assessment
-    if (autismRisk === 0 && dyslexiaRisk === 0){ return result; }
-
-    if (autismRisk > dyslexiaRisk){ result.disorder = 'AU'; result.riskLevel = autismRisk; }
-    else if (autismRisk < dyslexiaRisk){ result.disorder = 'DX'; result.riskLevel = dyslexiaRisk; }
-    else { result.disorder = "AU DX"; result.riskLevel = autismRisk; }
+    if (probability <= 25){ result.dyslexiaRL = 0; }
+    else if (probability > 25 && probability <= 50){ result.dyslexiaRL = 1; }
+    else if (probability > 50 && probability <=75){ result.dyslexiaRL = 2; }
+    else if (probability > 75){ result.dyslexiaRL = 3; }
 
     return result
 }
